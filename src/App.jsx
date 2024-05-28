628370b1667bf7ac134ecd02276e01ec5eaa4257
AhmedUmanlink <ahmed.jaidi@umanlink.com>
2024-05-28 10:03:54 +0100

    c0 : initial commuit , initialise vite react + setting up shadcn-ui

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { CarouselDemo } from "./components/demo/CarouselDemo"
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    <div className="px-20">
+
+    <Button>hello my friend </Button>
+    <CarouselDemo ></CarouselDemo>
+    </div>
+    </>
+  )
+}
+
+export default App

be50584d694f5c668c62a7811e29317b1b36ddcc
FOURTY-47-SEVEN <ahmedjaidi4@gmail.com>
2024-06-13 16:44:28 +0100

    timer

--- src/pages/mines-game/Mines.jsx
+++ src/pages/mines-game/Mines.jsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react";
 import bomb from "../../assets/bomb.gif";
 import flag from "../../assets/flag-2.png";
-import { Bomb, Flag } from "lucide-react";
+import {
+  Bomb,
+  Flag,
+  Gamepad2,
+  HelpCircle,
+  Keyboard,
+  Lightbulb,
+  ShieldQuestion,
+  Trophy,
+} from "lucide-react";
 import { useWindowSize } from "react-use";
 import Confetti from "react-confetti";
 import { Label } from "@/components/ui/label";
@@ -14,11 +23,23 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  
+  DialogTrigger,
 } from "@/components/ui/dialog";
 
-const MAX_ROWS_COLS = 10;
-
+const MAX_ROWS_COLS = 12;
+const rays= {
+    "2":"40",
+    "3":"20",
+    "4":"20",
+    "5":"20",
+    "6":"20",
+    "7":"20",
+    "8":"20",
+    "9":"10",
+    "10":"10",
+    "11":"10",
+    "12":"10",
+}
 export default function Mines() {
   const [board, setBoard] = useState([]);
   const [win, setWin] = useState(1);
@@ -28,11 +49,11 @@ export default function Mines() {
   const [modalOpen, setModalOpen] = useState(true);
   const { width, height } = useWindowSize();
 
-  const ray = 20; // Default ray value
 
   // Modal input states
   const [ROWS, setROWS] = useState(6);
   const [COLS, setCOLS] = useState(6);
+  const ray = rays[COLS]; // Default ray value
 
   useEffect(() => {
     initializeBoard(ROWS, COLS);
@@ -69,6 +90,7 @@ export default function Mines() {
         newBoard[rowIndex][colIndex].o = true;
         if (board[rowIndex][colIndex].v === "X") {
           setWin(2);
+          handlePause();
           newBoard.forEach((row) => row.forEach((cell) => (cell.o = true)));
         } else if (board[rowIndex][colIndex].v !== "X") {
           newBoard[rowIndex][colIndex].v = calculateNbrMines(
@@ -173,27 +195,58 @@ export default function Mines() {
       correctlyFlagged === nbrMines ||
       (uncoveredCells === ROWS * COLS - nbrMines && allBombsUnrevealed)
     ) {
+      handlePause();
       setWin(3); // 3 represents a win state
     }
   };
 
   const handleReset = () => {
     initializeBoard(ROWS, COLS);
-    // setModalOpen(true)
+    handleTimerReset();
+    handleStart();
+
   };
   const handleSubmitModal = () => {
-    if (ROWS > 0 && COLS > 0 && COLS <= MAX_ROWS_COLS) {
+    handleReset();
+    if (ROWS > 1 && COLS > 1 && COLS <= MAX_ROWS_COLS) {
       setModalOpen(false);
     } else {
       alert(`Please enter valid  columns (max ${MAX_ROWS_COLS}).`);
     }
   };
+/////////TIMER/////////
 
+
+  const [time, setTime] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+  useEffect(() => {
+    let interval
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1000)
+      }, 1000)
+    }
+    return () => clearInterval(interval)
+  }, [isRunning])
+  const handleStart = () => {
+    setIsRunning(true)
+  }
+  const handlePause = () => {
+    setIsRunning(false)
+  }
+  const handleTimerReset = () => {
+    setTime(0)
+    setIsRunning(false)
+  }
+  const minutes = Math.floor(time / 60000)
+  const seconds = Math.floor((time % 60000) / 1000)
+
+/////////TIMER/////////
   return (
     <div className="flex flex-col items-center justify-center min-h-screen pb-5">
-      <Dialog defaultOpen open={modalOpen} >
+      <Dialog defaultOpen open={modalOpen}>
         <DialogContent className="sm:max-w-[400px]">
-          <DialogHeader >
+          <DialogHeader>
             <DialogTitle>Create a game Grid</DialogTitle>
             <DialogDescription>
               Enter the number of rows and columns for your game grid.
@@ -208,7 +261,7 @@ export default function Mines() {
                 id="rows"
                 type="number"
                 min="2"
-                max="10"
+                max="12"
                 value={ROWS}
                 onChange={(e) => setROWS(parseInt(e.target.value))}
                 className="col-span-3"
@@ -222,7 +275,7 @@ export default function Mines() {
                 id="columns"
                 type="number"
                 min="2"
-                max="10"
+                max="12"
                 value={COLS}
                 onChange={(e) => setCOLS(parseInt(e.target.value))}
                 className="col-span-3"
@@ -237,34 +290,145 @@ export default function Mines() {
 
       <h1 className="text-4xl font-bold mb-4">Find Mines</h1>
 
-      <div className="text-lg mb-8">
-        <div>Number of Bombs: {nbrMines}</div>
-        <div>Available Flags: {nbrMines - nbrFlags}</div>
-      </div>
       <div className="flex items-center mb-4">
         <Button
-                   className={`px-4 py-2 rounded-r-none`}
-
+          className={`px-4 py-2 rounded-r-none`}
           onClick={() => setMode("reveal")}
-          variant={`${
-            mode === "reveal" ? "" : "secondary"
-          }`}
+          variant={`${mode === "reveal" ? "" : "secondary"}`}
         >
           Reveal
         </Button>
         <Button
           className={`px-4 py-2 rounded-l-none`}
-          variant={`${
-            mode === "flag" ? "" : "secondary"
-          }`}
+          variant={`${mode === "flag" ? "" : "secondary"}`}
           onClick={() => setMode("flag")}
         >
-          Flag 
+          Flag
         </Button>
-        <span className="bg-pink-800 h-full ml-5 px-5  rounded flex items-center gap-2 text-white"> <Flag /> {nbrMines - nbrFlags}</span>
-        <span className="bg-slate-800 h-full ml-5 px-5  rounded flex items-center gap-2 text-white"> <Bomb /> {nbrMines}</span>
+        <span className="bg-pink-800 h-full ml-5 px-5  rounded flex items-center gap-2 text-white">
+          <Flag /> {nbrMines - nbrFlags}
+        </span>
+        <span className="bg-slate-800 h-full ml-5 px-5  rounded flex items-center gap-2 text-white">
+          <Bomb /> {nbrMines}
+        </span>
+        <Dialog defaultOpen={false}>
+          <DialogTrigger asChild>
+            <Button variant="outline" className="ml-5 ">
+              <HelpCircle />
+            </Button>
+          </DialogTrigger>
+          
+          <DialogContent className="overflow-y-auto	 max-h-[75dvh] ">
+            <DialogHeader>
+              <DialogTitle className="flex items-center gap-1">
+                 
+                Objective <ShieldQuestion />
+              </DialogTitle>
+              <DialogDescription>
+                The objective of the game is to reveal all the safe cells
+                without hitting any mines. The game is won when all safe cells
+                are revealed, and all mines are correctly flagged.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogTitle className="flex items-center gap-1">
+              Gameplay
+              <Gamepad2 /> 
+            </DialogTitle>
+            <ul className="list-disc  px-5">
+              <DialogDescription className="mb-1 list-item">
+                The game board consists of a grid of cells, each of which may
+                contain a mine or be safe.
+              </DialogDescription>
+              <DialogDescription className="mb-1 list-item">
+                The number of mines on the board is displayed at the top of the
+                screen.
+              </DialogDescription>
+              <DialogDescription className="mb-1 list-item">
+                You can toggle between two modes: "Reveal" and "Flag".
+              </DialogDescription>
+              <DialogDescription className="mb-1 list-item">
+                In "Reveal" mode, clicking a cell will reveal its content. If
+                the cell is safe, it will display a number indicating how many
+                mines are adjacent to it (including diagonally adjacent cells).
+                If the cell has no adjacent mines, it will be blank, and all the
+                surrounding safe cells will be automatically revealed.
+              </DialogDescription>
+              <DialogDescription className="mb-1 list-item">
+                If you reveal a cell containing a mine, the game is over, and
+                all mines will be shown on the board.
+              </DialogDescription>
+              <DialogDescription className="mb-1 list-item">
+                In "Flag" mode, clicking a cell will toggle a flag on that cell
+                . You can use flags to mark the cells you suspect contain mines.
+                The number of available flags is displayed at the top of the
+                screen and is equal to the number of mines on the board.
+              </DialogDescription>
+            </ul>
+            <DialogTitle className="flex items-center gap-1">
+               
+              Winning <Trophy />
+            </DialogTitle>
+            <DialogDescription >
+              You win the game when all safe cells have been revealed, and all
+              mines have been correctly flagged.
+            </DialogDescription>
+            <DialogTitle className="flex items-center gap-1">
+               
+              Controls <Keyboard />
+            </DialogTitle>
+            <ul className="list-disc  px-5">
+              <DialogDescription className="mb-1 list-item">
+                On Desktop:
+                <DialogDescription className="ms-2 list-item list-none">
+                 - Left-click a cell to reveal it or toggle a flag (depending on
+                  the current mode).
+                </DialogDescription>
+              </DialogDescription>
+              <DialogDescription className="mb-1 list-item">
+              On Mobile/Tablet:
+                <DialogDescription className="ms-2 list-item list-none">
+                -   Tap a cell to reveal it or toggle a flag (depending on the current mode).
+                </DialogDescription>
+              </DialogDescription>
+              <DialogDescription className="mb-1 list-item">
+              Use the "Reveal" and "Flag" buttons to switch between modes.
+                
+              </DialogDescription>
+              <DialogDescription className="mb-1 list-item">
+              Use the "Reveal" and "Flag" buttons to switch between modes.
+               
+              </DialogDescription>
+            </ul>
+
+
+            <DialogTitle className="flex items-center gap-1">
+               
+            Tips <Lightbulb />
+             </DialogTitle>
+             <ul className="list-disc  px-5">
+               <DialogDescription className="mb-1 list-item">
+               Pay close attention to the numbers revealed on safe cells. They will help you deduce the locations of mines.                
+               </DialogDescription>
+               
+               <DialogDescription className="mb-1 list-item">
+               Use flags wisely. If you run out of flags, you won't be able to mark any remaining mines.
+                 
+               </DialogDescription>
+               <DialogDescription className="mb-1 list-item">
+               If you're stuck, try revealing cells near the edges of the board or cells with high adjacent mine counts.
+
+                
+               </DialogDescription>
+             </ul>
+
+          </DialogContent>
+        </Dialog>
       </div>
-      <div className={`grid grid-cols-${COLS} gap-2`}>
+      <div className="text-4xl font-bold border rounded mb-2 py-1 md:px-12 px-3">
+        {minutes.toString().padStart(2, "0")}:{seconds.toString().padStart(2, "0")}
+      </div>
+
+      <div className={`grid grid-cols-${COLS} gap-1`}>
         {board.map((row, rowIndex) =>
           row.map((cell, colIndex) => (
             <button
@@ -310,21 +474,16 @@ export default function Mines() {
         </div>
       )}
       <div className="my-5 w-full flex items-center justify-center gap-5">
-      <Button
-        
-        onClick={handleReset}
-      >
-        Reset
-      </Button>
+        <Button onClick={handleReset}>Reset</Button>
 
-      <Button
-        variant="secondary"
-        onClick={()=>{
-          handleReset() ;
-          setModalOpen(true)}}
-      >
-        Restart 
-      </Button>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            setModalOpen(true);
+          }}
+        >
+          Restart
+        </Button>
       </div>
     </div>
   );
